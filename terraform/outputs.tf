cf94e51297f3710f4ca0c7ddd9f31a43992fe749
Justin <jwong47@u.rochester.edu>
2025-08-30 02:44:29 -0400

    Minor fixes to some outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -13,32 +13,32 @@ output "www_website_url" {
 #   value       = aws_cloudfront_distribution.s3.id
 # }
 
-# output "cloudfront_domain_name" {
-#   description = "CloudFront distribution domain name"
-#   value       = aws_cloudfront_distribution.s3.domain_name
-# }
+output "cloudfront_domain_name" {
+  description = "CloudFront distribution domain name"
+  value       = module.cloudfront.cloudfront_domain_name
+}
 
-# output "s3_bucket_name" {
-#   description = "Name of the S3 bucket"
-#   value       = aws_s3_bucket.this.id
-# }
+output "s3_bucket_name" {
+  description = "Name of the S3 bucket"
+  value       = module.s3.bucket_id
+}
 
-# output "s3_bucket_arn" {
-#   description = "ARN of the S3 bucket"
-#   value       = aws_s3_bucket.this.arn
-# }
+output "s3_bucket_arn" {
+  description = "ARN of the S3 bucket"
+  value       = module.s3.bucket_arn
+}
 
 output "route53_zone_id" {
   description = "Route 53 hosted zone ID"
-  value       = aws_route53_zone.main.zone_id
+  value       = module.route53.zone_id
 }
 
-output "route53_name_servers" {
-  description = "Route 53 name servers"
-  value       = aws_route53_zone.main.name_servers
-}
+# output "route53_name_servers" {
+#   description = "Route 53 name servers"
+#   value       = aws_route53_zone.main.name_servers
+# }
 
 output "acm_certificate_arn" {
   description = "ARN of the ACM certificate"
-  value       = aws_acm_certificate.this.arn
+  value       = module.acm.certificate_arn
 }
