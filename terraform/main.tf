c3785bf71956c7f758f8166269966b2df8ccdd83
Justin <jwong47@u.rochester.edu>
2025-08-30 01:31:09 -0400

    Set up route_53 folder and removedold cloudfront stuff from main

--- terraform/main.tf
+++ terraform/main.tf
@@ -56,50 +56,6 @@ resource "aws_acm_certificate_validation" "cert_validation" {
   validation_record_fqdns = [for record in aws_route53_record.acm_validation : record.fqdn]
 }
 
-resource "aws_cloudfront_distribution" "s3" {
-  origin {
-    domain_name              = aws_s3_bucket.this.bucket_regional_domain_name
-    origin_id                = local.s3_origin_id
-    origin_access_control_id = aws_cloudfront_origin_access_control.s3_access.id
-  }
-
-  aliases = [var.domain_name, "www.${var.domain_name}"]
-
-  enabled             = true
-  is_ipv6_enabled     = true
-  default_root_object = "index.html"
-  price_class         = "PriceClass_100"
-
-  default_cache_behavior {
-    target_origin_id       = local.s3_origin_id
-    allowed_methods        = ["GET", "HEAD"]
-    cached_methods         = ["GET", "HEAD"]
-    viewer_protocol_policy = "redirect-to-https"
-
-    cache_policy_id = "658327ea-f89d-4fab-a63d-7e88639e58f6"
-  }
-
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
-
-  viewer_certificate {
-    acm_certificate_arn      = aws_acm_certificate_validation.cert_validation.certificate_arn
-    ssl_support_method       = "sni-only"
-    minimum_protocol_version = "TLSv1.2_2021"
-  }
-}
-
-resource "aws_cloudfront_origin_access_control" "s3_access" {
-  name                              = "${var.project_name}-oac"
-  description                       = "OAC for cloudfront access to s3"
-  origin_access_control_origin_type = "s3"
-  signing_behavior                  = "always"
-  signing_protocol                  = "sigv4"
-}
-
 resource "aws_route53_zone" "main" {
   name = var.domain_name
 
