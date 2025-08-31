8b015ff6542c6c9785f169c678974f65db7fc362
Justin <jwong47@u.rochester.edu>
2025-08-31 03:37:38 -0400

    Made ACM certificate use us-east-1

--- terraform/main.tf
+++ terraform/main.tf
@@ -46,6 +46,7 @@ module "cloudfront" {
 }
 
 resource "aws_acm_certificate" "this" {
+  provider                  = aws.us-east-1
   domain_name               = var.domain_name
   subject_alternative_names = ["*.${var.domain_name}"]
   validation_method         = "DNS"
